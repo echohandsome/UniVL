58a67f24a3839984f5551b8828202c45a43a8513
python_Data <35515187+echohandsome@users.noreply.github.com>
2022-09-28 15:55:01 +0800

    Update module_bert.py
    添加笔记内容

--- modules/module_bert.py
+++ modules/module_bert.py
@@ -98,7 +98,7 @@ class BertConfig(PretrainedConfig):
             with open(vocab_size_or_config_json_file, "r", encoding='utf-8') as reader:
                 json_config = json.loads(reader.read())
             for key, value in json_config.items():
-                self.__dict__[key] = value
+                self.__dict__[key] = value                                                       # 这种方法可以让类在实例化成为对象后，对象的__dict__方法直接形成结果
         elif isinstance(vocab_size_or_config_json_file, int):
             self.vocab_size = vocab_size_or_config_json_file
             self.hidden_size = hidden_size
